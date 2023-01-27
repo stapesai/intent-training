58992a66e2b72f08f8c6bda470e54ba8d6843e76
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2023-01-28 00:05:13 +0530

    added some json for volume control

---
+++ data/raw/volume/scripts/remove_duplicates_from_raw.py
@@ -0,0 +1,23 @@
+# Description: This script removes duplicate lines from the raw data files
+
+inp_file = 'data/raw/volume/set_by_value.txt'
+out_file = 'data/raw/volume/set_by_value.txt'
+
+with open(inp_file, 'r') as f:
+    text = f.readlines()
+
+# print((text))
+# print(str(text[0]))
+
+print('len of original list: ', len(text))
+
+lines = set()
+for line in text:
+    line = line.strip().lower()
+    lines.add(line)
+
+print('len of set: ', len(lines))
+
+with open(out_file, 'w') as f:
+    for line in lines:
+        f.write(line + '\n')
